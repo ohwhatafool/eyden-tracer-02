5fe30be746334d73c334dd635acf774990baa3f7
Drish10 <drishti.serenity@gmail.com>
2019-10-11 01:37:37 +0200

    Done LightPoint.h

--- src/LightPoint.h
+++ src/LightPoint.h
@@ -14,19 +14,27 @@ public:
 	 * @param intensity The emission color and strength of the light source
 	 */
 	CLightPoint(Vec3f intensity, Vec3f position)
-		: m_intensity(intensity)
-		, m_position(position)
-	{} 
+		: m_intensity(intensity), m_position(position)
+	{
+	}
 	virtual ~CLightPoint(void) = default;
 
-	virtual std::optional<Vec3f> Illuminate(Ray& ray) override
+	virtual std::optional<Vec3f> Illuminate(Ray &ray) override
 	{
 		// --- PUT YOUR CODE HERE ---
-		
+		//Document referenced to get the light intensity is:
+		//https://computergraphics.stackexchange.com/questions/5166/how-to-find-light-direction-from-a-point-on-a-surface-to-an-area-light
 
-		return Vec3f();
+		Vec3f intensity;
+		Vec3f directionLight = m_position - ray.org;
+
+		intensity = m_intensity / pow(cv::norm(directionLight), 2);
+
+		ray.dir = normalize(directionLight);
+
+		//normalized form is v important
+		return normalize(intensity);
 	}
-
 
 private:
 	Vec3f m_intensity; ///< emission (red, green, blue)
